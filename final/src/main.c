2171b4c814d126ea68df874779117867713990f6
David Andres Yanez Martinez <dyanez-m@c1r17s2.42madrid.com>
2024-09-02 15:25:04 +0200

    main reconstruido

--- final/src/main.c
+++ final/src/main.c
@@ -3,14 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: david <david@student.42.fr>                +#+  +:+       +#+        */
+/*   By: dyanez-m <dyanez-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/31 12:40:23 by david             #+#    #+#             */
-/*   Updated: 2024/08/31 18:33:02 by david            ###   ########.fr       */
+/*   Updated: 2024/09/02 15:22:32 by dyanez-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headers/minishell.h"
+
+int g_error;
 
 static void	init_mini(t_mini *mini, char **envp)
 {
@@ -19,16 +21,27 @@ static void	init_mini(t_mini *mini, char **envp)
 	mini->table = NULL;
 	mini->envp = NULL;
 	mini->exit_status = 0;
+	mini->error = 0;
 	mini->exec_envp = NULL;
 	envp_init(&mini->envp, envp);
 	mini->exec_envp = copy_double_str(envp);
 }
 
+static void	iteration_handler(t_mini *mini)
+{
+	free_token_list(&mini->tokens);
+	free_table(&mini->table);
+	free(mini->line);
+	mini->line = NULL;
+	mini->tokens = NULL;
+	mini->table = NULL;
+	mini->error = 0;
+}
 static void	ft_loop(t_mini *mini)
 {
 	while (1)
 	{
-		//setup_signal_handlers();
+		while_signals(mini);
 		mini->line = readline("msh>>");
 		if (!mini->line)
 		{
@@ -39,8 +52,14 @@ static void	ft_loop(t_mini *mini)
 		add_history(mini->line);
 		tokenize_line(mini->line, mini);
 		if (mini->error == 0)
+		{	
 			parser_token(mini);
-
+			process_heredoc(mini);
+			do_redir_handler(mini);
+		}
+		if (mini->error == 0)
+			execute(mini);
+		iteration_handler(mini);
 	}
 }
 int	main(int argc, char **argv, char **envp)
