c00bdd9ef711834abb44335913aecc33c6dec4b8
Jonathan Jimenez Munoz <jonjimen@c1r17s1.42madrid.com>
2024-09-02 15:10:59 +0200

    main.c

---
+++ final/src/main.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: david <david@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/31 12:40:23 by david             #+#    #+#             */
+/*   Updated: 2024/08/31 18:33:02 by david            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../headers/minishell.h"
+
+static void	init_mini(t_mini *mini, char **envp)
+{
+	mini->line = NULL;
+	mini->tokens = NULL;
+	mini->table = NULL;
+	mini->envp = NULL;
+	mini->exit_status = 0;
+	mini->exec_envp = NULL;
+	envp_init(&mini->envp, envp);
+	mini->exec_envp = copy_double_str(envp);
+}
+
+static void	ft_loop(t_mini *mini)
+{
+	while (1)
+	{
+		//setup_signal_handlers();
+		mini->line = readline("msh>>");
+		if (!mini->line)
+		{
+			printf("msh>>exit\n");
+			ft_free_all(mini);
+			exit(mini->exit_status);
+		}
+		add_history(mini->line);
+		tokenize_line(mini->line, mini);
+		if (mini->error == 0)
+			parser_token(mini);
+
+	}
+}
+int	main(int argc, char **argv, char **envp)
+{
+	t_mini	mini;
+	(void)argc;
+	(void)argv;
+
+	init_mini(&mini, envp);
+	ft_loop(&mini);
+}
